4a65ba6bae6f1a2f0092e5e7fdfe65551a9abb57
ZeldaLord <83875893+ZeldaLord1@users.noreply.github.com>
2025-10-03 23:10:37 +0200

    Add support for aiming with left analog stick

--- script/shotgun.gd
+++ script/shotgun.gd
@@ -2,8 +2,15 @@ extends Node2D
 
 const bullet = preload("res://scene/bullet.tscn")
 var BulletCooldown
+var mouse_pos
+var cursor_velocity = Vector2.ZERO
 
-func _process(_delta):
+func _process(delta):
+	cursor_velocity = Input.get_vector("mouse_left", "mouse_right", "mouse_up", "mouse_down")
+	cursor_velocity = cursor_velocity.limit_length()
+	mouse_pos = get_viewport().get_mouse_position()
+	Input.warp_mouse(mouse_pos + cursor_velocity * 2500 * delta)
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
 	look_at(get_global_mouse_position())
 	rotation_degrees = wrap(rotation_degrees, 0, 360)
 	if rotation_degrees > 90 and rotation_degrees < 270:
