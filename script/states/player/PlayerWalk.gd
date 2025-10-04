4eb1c4efb9841e784dc678415a83fd5369c87cb0
Pixelated Face <105989373+PixelatedFace@users.noreply.github.com>
2025-10-04 05:49:53 +0300

    i stayed up till 5 am making this game look good please fix movement thanksssssss

--- script/states/player/PlayerWalk.gd
+++ script/states/player/PlayerWalk.gd
@@ -1,28 +1,6 @@
-class_name PlayerWalk
+class_name PlayerTalk
 extends State
 
 
 @export var player: Player
 var DashCooldown
-func enter():
-	player.sprite.play("walk")
-
-func physics_update(_delta: float):
-	
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		player.velocity.x = direction * player.speed
-	else:
-		player.velocity.x = move_toward(player.velocity.x, 0, player.speed)
-		player.state_machine.change_state("Idle")
-
-func input(_event):
-	if Input.is_action_just_pressed("jump"):
-		player.state_machine.change_state("Jump")
-	if Input.is_action_just_pressed("dash"):
-		if not DashCooldown:
-			player.state_machine.change_state("Dash")
-			DashCooldown = true
-			$"../Dash/DashCooldown".start()
-func DashCooldownOut():
-	DashCooldown = false
