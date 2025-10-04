d511e903d3d7a0073746c98e84d0e703e0554a76
Kuudraloremaster <KuudraLoremaster@users.noreply.github.com>
2025-10-04 14:14:09 +0300

    Egyptian runes show real important text

--- script/player.gd
+++ script/player.gd
@@ -5,8 +5,15 @@ extends CharacterBody2D
 var speed = 150
 var jump_vel = -200
 var jumps_left = 2
+
+var in_sign = false
+var dialogue_shown = false
+
 @export var sprite: AnimatedSprite2D
 @export var state_machine: StateMachine
+@onready var area_2d: Area2D = $"../BGTiles/Sign1BG/Sign1/Area2D"
+const DIALOGUE = preload("uid://dy8hu8mwduyi2")
+@onready var main: Node2D = $".."
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -21,3 +28,19 @@ func _physics_process(delta: float) -> void:
 		$Sprite.flip_h = false
 	elif velocity.x < 0:
 		$Sprite.flip_h = true
+	if in_sign:
+		if Input.is_action_just_pressed("interact") and not dialogue_shown:
+			dialogue_shown = true
+			var dialogue_instance:Node2D = DIALOGUE.instantiate()
+			main.add_child(dialogue_instance)
+			dialogue_instance.position = Vector2(452.0, 555.0)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		in_sign =  true
+	
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Player:
+		in_sign = false
