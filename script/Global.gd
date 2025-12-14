a34335750bb67f38ae5ba50040863651f0c7dbe0
ZeldaLord <83875893+ZeldaLord1@users.noreply.github.com>
2025-12-14 01:54:06 +0100

    Inputs & Remove the txt thing

--- script/Global.gd
+++ script/Global.gd
@@ -5,9 +5,9 @@ const BulletDamageUp = 50
 
 @onready var player: Player = get_tree().get_first_node_in_group("Player")
 
-func write_dialogue():
-	OS.shell_open("notepad.exe")
-	await get_tree().create_timer(0.5).timeout
-	get_window().grab_focus() # maaan
-	OS.alert("Then Write.")
-	
+# func write_dialogue():
+	#OS.shell_open("notepad.exe")
+	#await get_tree().create_timer(0.5).timeout
+	#get_window().grab_focus() # maaan
+	#OS.alert("Then Write.")
+# Let's not. (Maybe (Unless there will be a good idea (There won't be (Most likely))))
