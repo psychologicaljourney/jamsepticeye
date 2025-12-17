368a7ffa485fbfd4f175bc82c4fd16f0d113ca3d
Pixelated Face <105989373+PixelatedFace@users.noreply.github.com>
2025-12-17 03:18:59 +0200

    :steamhappy:

--- script/living.gd
+++ script/living.gd
@@ -1,15 +1,36 @@
 class_name Living
 extends CharacterBody2D
 
-
+@export var sprite: Sprite2D
 @export var max_health := 100.0
 @onready var health = max_health
 
+var hit_effect_timer: Timer
+
 signal health_changed
+
+func _ready() -> void:
+	hit_effect_timer = Timer.new()
+	hit_effect_timer.one_shot = true
+	hit_effect_timer.wait_time = 0.2
+	hit_effect_timer.timeout.connect(reset_hit_effect)
+	add_child(hit_effect_timer)
 
 func damage(amount: float):
 	set_health(health-amount)
+	hit_anim()
 
+func reset_hit_effect():
+	sprite.set_instance_shader_parameter("hit", false)
+	sprite.set_instance_shader_parameter("hit_color", Color(1,1,1,1))
+	
+func set_hit_color(col: Color):
+	sprite.set_instance_shader_parameter("hit_color", col)
+
+func hit_anim():
+	sprite.set_instance_shader_parameter("hit", true)
+	hit_effect_timer.start()
+	
 func heal(amount: float):
 	set_health(health+amount)
 
--- script/weapons/rifle_target.gd
+++ script/weapons/rifle_target.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 	await get_tree().create_timer(1).timeout
 	$CPUParticles2D.emitting = true
 	var l: Living = get_parent()
+	l.set_hit_color(Color(1.0, 0.0, 0.0, 1.0))
 	for i in range(40):
 		l.damage(1)
 		await get_tree().create_timer(0.04).timeout
